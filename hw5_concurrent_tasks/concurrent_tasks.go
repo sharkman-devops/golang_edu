a0b6471c9a4b53d8376113ae157a9c8c9953221b
Andrey Okulov <okulov@ya.ru>
2020-01-26 23:40:08 +0300

    [add] new version of concurrent_tasks

--- hw5_concurrent_tasks/concurrent_tasks.go
+++ hw5_concurrent_tasks/concurrent_tasks.go
@@ -4,82 +4,74 @@ import (
 	"errors"
 	"fmt"
 	"runtime"
+	"sync"
 	"time"
 )
 
 // Run function runs functions from `[]func() error` with N concurrency.
 // Returns an error if more than M functions return errors
 func Run(task []func() error, N int, M int) error {
-	queueChan := make(chan bool, N)
+	taskChan := make(chan func() error, len(task))
 	shutdownChan := make(chan bool)
 	errorsChan := make(chan bool, len(task))
-	finishChan := make(chan bool, len(task))
-	resultChan := make(chan bool)
+	waitGroup := sync.WaitGroup{}
+	workersPool := N
+	if len(task) < N {
+		workersPool = len(task)
+	}
 
 	if M < 1 || N < 1 {
 		return errors.New("wrong arguments")
 	}
 
-	go func(shutdown chan bool, errors <-chan bool, finish <-chan bool, result chan bool) {
-		errorsCounter := 0
-		finishCounter := 0
-		for {
-			select {
-			case <-errors:
-				errorsCounter++
-				if errorsCounter == M {
-					close(shutdown)
-				}
-			case <-finish:
-				finishCounter++
-				if finishCounter == len(task) {
-					select {
-					case <-shutdown:
-						for {
-							if len(queueChan) == 0 {
-								break
-							}
-						}
-						result <- false
-						return
-					default:
-						result <- true
-						return
-					}
-				}
-			}
-		}
-	}(shutdownChan, errorsChan, finishChan, resultChan)
-
 	for _, t := range task {
-		queueChan <- true
-		go wrapper(t, queueChan, errorsChan, finishChan, shutdownChan)
+		taskChan <- t
+	}
+	close(taskChan)
+
+	for i := 0; i < workersPool; i++ {
+		waitGroup.Add(1)
+		go worker(taskChan, &waitGroup, errorsChan, shutdownChan, M)
 	}
 
-	result := <-resultChan
-	if result {
-		return nil
+	waitGroup.Wait()
+	if len(errorsChan) >= M {
+		return errors.New("too many errors")
 	}
-	return errors.New("too many errors")
-
+	return nil
 }
 
-func wrapper(f func() error, queueChan <-chan bool, errorChan chan<- bool, finishChan chan<- bool, shutdown <-chan bool) {
-	select {
-	default:
-		err := f()
-		if err != nil {
-			errorChan <- true
-			time.Sleep(100 * time.Millisecond) // time to prevent race condition
-			finishChan <- true
-		} else {
-			finishChan <- true
+func worker(tasks <-chan func() error, wg *sync.WaitGroup, errors chan<- bool, shutdown <-chan bool, maxErrors int) {
+	for {
+		select {
+		case <-shutdown:
+			wg.Done()
+			return
+		default:
 		}
-	case <-shutdown:
-		finishChan <- true
-	}
 
-	<-queueChan
+		select {
+		case f, ok := <-tasks:
+			if !ok {
+				wg.Done()
+				return
+			}
+
+			if len(errors) >= maxErrors {
+				wg.Done()
+				return
+			}
+
+			err := f()
+			if err != nil {
+				errors <- true
+			}
+
+		case <-shutdown:
+			wg.Done()
+			return
+		}
+	}
 }
 
 func nilFunc() error {
@@ -102,9 +94,9 @@ func errorFunc() error {
 }
 
 func main() {
-	tasks := []func() error{errorFunc, longNilFunc}
+	tasks := []func() error{nilFunc, errorFunc, errorFunc}
 	fmt.Println(runtime.NumGoroutine())
-	err := Run(tasks, 20, 12)
+	err := Run(tasks, 1, 1)
 	fmt.Println(runtime.NumGoroutine())
 	fmt.Println(err)
 }
